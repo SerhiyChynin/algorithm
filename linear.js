8d4d22e5fea34568b5b04e46431679089de497e8
SerhiiChynin <sergey.chinin@gmail.com>
2023-08-10 16:27:25 +0300

    linearSearch

--- linear.js
+++ linear.js
@@ -1,5 +1,15 @@
 const array = [1, 4, 7, 6, 5, 6, 2, 9, 8, 3, 4];
+let count = 0;
 
-function linearSearch() {
-    
+function linearSearch(array, item) {
+    for (let i = 0; i < array.length; i++){
+        count += 1;
+        if (array[i] === item) {
+            return i;
+        }
+    }
+    return null;
 }
+
+console.log(linearSearch(array, 31));
+console.log('count:', count);
