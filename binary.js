d2f4e8e1c2b8232b3d519d39a9b08c3746be0d36
SerhiiChynin <sergey.chinin@gmail.com>
2023-08-11 14:11:24 +0300

    binarySearch

--- binary.js
+++ binary.js
@@ -1,8 +1,28 @@
-const array = [1, 4, 7, 6, 5, 6, 2, 9, 8, 3, 4];
+const array = [1, 4, 7, 6, 5, 6, 2, 9, 8, 3, 4,10,11,12,13,14];
 let count = 0;
 
 function binarySearch(array, item) {
     let start = 0;
+    let end = array.length;
+    let middle;
+    let found = false;
+    let position = -1;
+    while (found === false && start <= end) {
+        count += 1
+        middle = Math.floor((start + end) / 2);
+        if (array[middle] === item) {
+            found = true;
+            position = middle;
+            return position;
+        }
+        if (item < array[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+    }
+    return position;
 }
 
+console.log(binarySearch(array, 3));
 console.log(count);
