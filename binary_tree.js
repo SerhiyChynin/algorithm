b4561eeec14c2f043c34a8b71b24eac35a7a2ea8
SerhiiChynin <sergey.chinin@gmail.com>
2023-08-16 11:42:36 +0300

    binary_tree

--- binary_tree.js
+++ binary_tree.js
@@ -7,9 +7,38 @@ class BinaryTree{
             this.root = new TreeNode(value);
         } else {
             let node = this.root
+            let newNode = new TreeNode(value)
+            while (node) {
+                if (value > node.value) {
+                    if (!node.right) {
+                        break
+                    }
+                    node = node.right;
+                } else {
+                      if (!node.left) {
+                        break
+                    }
+                    node = node.left;
+                }
+            }
+            if (value > node.value) {
+                node.right = newNode;
+            } else {
+                node.left = newNode;
+            }
         }
     }
+    print(root = this.root) {
+        if (!root) {
+            return true;
+        }
+        console.log(root.value);
+        this.print(root.left);
+        this.print(root.right);
+    }
 }
+
+
 
 class TreeNode{
     constructor(value) {
@@ -19,3 +48,12 @@ class TreeNode{
     }
 
 }
+const tree = new BinaryTree()
+tree.add(5)
+tree.add(4)
+tree.add(3)
+tree.add(6)
+tree.add(4)
+tree.add(8)
+tree.add(1)
+tree.print()
